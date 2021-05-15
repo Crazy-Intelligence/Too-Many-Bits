95bc01c3bcf26804f57b6c3fcaa47d565a9f6969
Jasch0Jumper <ja.coding7@gmail.com>
2021-05-15 13:44:08 +0200

    created GameEventListener to replace individual GameEvent Scripts

---
+++ Unity/Too Many Bits/Assets/_Project/Scripts/TooManyBits/GameEventListener.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace CrazyIntelligence.TooManyBits
+{
+	public class GameEventListener : MonoBehaviour
+	{
+		[SerializeField] private Sequence OnStart;
+		[SerializeField] private Sequence OnReset;
+		[SerializeField] private Sequence OnPause;
+		[SerializeField] private Sequence OnContinue;
+		[SerializeField] private Sequence OnGameOver;
+		[SerializeField] private Sequence OnAppStart;
+		[SerializeField] private Sequence OnAppExit;
+
+		private void OnEnable()
+		{
+			GameManager.OnStart += InvokeOnStart;
+			GameManager.OnReset += InvokenReset;
+			GameManager.OnPause += InvokeOnPause;
+			GameManager.OnContinue += InvokeOnContinue;
+			GameManager.OnGameOver += InvokeOnGameOver;
+			GameManager.OnAppStart += InvokeOnAppStart;
+			GameManager.OnAppExit += InvokeOnAppExit;
+		}
+		private void OnDisable()
+		{
+			GameManager.OnStart -= InvokeOnStart;
+			GameManager.OnReset -= InvokenReset;
+			GameManager.OnPause -= InvokeOnPause;
+			GameManager.OnContinue -= InvokeOnContinue;
+			GameManager.OnGameOver -= InvokeOnGameOver;
+			GameManager.OnAppStart -= InvokeOnAppStart;
+			GameManager.OnAppExit -= InvokeOnAppExit;
+		}
+
+		private void InvokeOnStart() => OnStart.Start();
+		private void InvokenReset() => OnReset.Start();
+		private void InvokeOnPause() => OnPause.Start();
+		private void InvokeOnContinue() => OnContinue.Start();
+		private void InvokeOnGameOver() => OnGameOver.Start();
+		private void InvokeOnAppStart() => OnAppStart.Start();
+		private void InvokeOnAppExit() => OnAppExit.Start();
+	}
+}
--- Unity/Too Many Bits/Assets/_Project/Scripts/TooManyBits/Sequence.cs
+++ Unity/Too Many Bits/Assets/_Project/Scripts/TooManyBits/Sequence.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace CrazyIntelligence.TooManyBits
